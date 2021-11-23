fc019e69cab169c3fece01da0d4ece58af00308b
finian1 <38808702+finian1@users.noreply.github.com>
2021-11-23 02:39:51 +0000

    Switch to CharacterController

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -17,7 +17,14 @@ public class PlayerController : MonoBehaviour
     public bool allowCameraMovement = true;
 
     private Rigidbody rb;
+    private CharacterController characterController;
 
+    //private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    /*private float playerSpeed = 10.0f;
+    private float jumpHeight = 1.0f;*/
+    private float gravityValue = -20f;
 
 
 
@@ -26,6 +33,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        
         spawnPosition = gameObject.transform.position;
         //Cursor.lockState = CursorLockMode.Locked;
     }
@@ -35,7 +44,32 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         
     }
+    private void Update()
+    {
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
+        Vector3 move = new Vector3((transform.forward * Input.GetAxisRaw("Vertical")).x + (transform.right * Input.GetAxisRaw("Horizontal")).x, 0, (transform.forward * Input.GetAxisRaw("Vertical")).z + (transform.right * Input.GetAxisRaw("Horizontal")).z);
+        move.Normalize();
+        characterController.Move(move * Time.deltaTime * movementSpeed);
+
+        /*if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }*/
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpForce * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        
+    }
 
     void FixedUpdate()
     {
@@ -45,6 +79,7 @@ public class PlayerController : MonoBehaviour
             Jump();
             MoveCamera();
             MovePlayer();
+            characterController.Move(playerVelocity * Time.deltaTime);
             /*if (Input.GetKey(KeyCode.B))
             {
                 allowCameraMovement = false;
@@ -119,7 +154,7 @@ public class PlayerController : MonoBehaviour
 
     private void MovePlayer()
     {
-        Vector3 directionVector = new Vector3(0, 0, 0);
+        /*Vector3 directionVector = new Vector3(0, 0, 0);
         if (Input.GetAxisRaw("Vertical") != 0)
         {
             directionVector += transform.forward * Input.GetAxisRaw("Vertical");
@@ -129,28 +164,44 @@ public class PlayerController : MonoBehaviour
             directionVector += transform.right * Input.GetAxisRaw("Horizontal");
         }
         directionVector.Normalize();
-        rb.MovePosition(transform.position + directionVector * Time.deltaTime * movementSpeed);
+        //rb.MovePosition(transform.position + directionVector * Time.deltaTime * movementSpeed);
         //rb.velocity = new Vector3(directionVector.x * Time.deltaTime * movementSpeed, rb.velocity.y, directionVector.z * Time.deltaTime * movementSpeed);
-
+        characterController.Move(directionVector * Time.deltaTime * movementSpeed);
+        characterController.Move(new Vector3(0, gravityValue, 0) * Time.deltaTime);*/
+        Vector3 move = new Vector3((transform.forward * Input.GetAxisRaw("Vertical")).x + (transform.right * Input.GetAxisRaw("Horizontal")).x, 0, (transform.forward * Input.GetAxisRaw("Vertical")).z + (transform.right * Input.GetAxisRaw("Horizontal")).z);
+        move.Normalize();
+        characterController.Move(move * Time.deltaTime * movementSpeed);
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
     }
 
     private void Jump()
     {
+        float offsetX = 0.48f;
+        float offsetZ = 0.48f;
         if (Input.GetKey(KeyCode.Space))
         {
             int layerMask = 1 << 8;
             layerMask = ~layerMask;
             RaycastHit hit;
+            /*groundedPlayer = (Physics.Raycast(transform.position, -Vector3.up, out hit, 1.05f, layerMask) ||
+                Physics.Raycast(new Vector3(transform.position.x + offsetX, transform.position.y, transform.position.z + offsetZ), -Vector3.up, out hit, 1.1f, layerMask) ||
+                Physics.Raycast(new Vector3(transform.position.x - offsetX, transform.position.y, transform.position.z + offsetZ), -Vector3.up, out hit, 1.1f, layerMask) ||
+                Physics.Raycast(new Vector3(transform.position.x + offsetX, transform.position.y, transform.position.z - offsetZ), -Vector3.up, out hit, 1.1f, layerMask) ||
+                Physics.Raycast(new Vector3(transform.position.x - offsetX, transform.position.y, transform.position.z - offsetZ), -Vector3.up, out hit, 1.1f, layerMask)
+                );*/
+
+            groundedPlayer = characterController.isGrounded;
+            /*if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }*/
 
             Debug.Log("Attempting Jump");
-            if (Physics.Raycast(transform.position, -Vector3.up, out hit, 1.05f, layerMask) ||
-                Physics.Raycast(new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z + 0.5f), -Vector3.up, out hit, 1.05f, layerMask) ||
-                Physics.Raycast(new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z + 0.5f), -Vector3.up, out hit, 1.05f, layerMask) ||
-                Physics.Raycast(new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z - 0.5f), -Vector3.up, out hit, 1.05f, layerMask) ||
-                Physics.Raycast(new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z - 0.5f), -Vector3.up, out hit, 1.05f, layerMask)
-                )
+            Debug.Log(groundedPlayer);
+            if (groundedPlayer)
             {
-                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z); ;
+                playerVelocity.y += Mathf.Sqrt(jumpForce * -3.0f * gravityValue);
             }
         }
 
