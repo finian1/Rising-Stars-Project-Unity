cb1f6985bec4177b40eb2a720dc4b264e5c5f26e
finian1 <38808702+finian1@users.noreply.github.com>
2022-01-05 18:17:03 +0000

    Buff system update

--- Assets/Resources/Scripts/PowerupScripts/Status_Base.cs
+++ Assets/Resources/Scripts/PowerupScripts/Status_Base.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Status_Base : MonoBehaviour
 {
     public string buffName;
-    protected float durationOfEffect;
-    protected float strengthOfEffect;
+    [SerializeField] protected Color collectibleColour;
+    [SerializeField] protected float durationOfEffect;
+    [SerializeField] protected float strengthOfEffect;
+    [SerializeField] protected GameObject internalIcon;
 
     private float buffTimer;
     private bool isActive = false;
     protected GameObject playerObject;
     protected PlayerController playerController;
+
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().material.color = collectibleColour;
+
+    }
 
     private void Update()
     {
@@ -23,6 +31,21 @@ public class Status_Base : MonoBehaviour
 
     public virtual void StartBuff(GameObject player)
     {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        if(renderer != null)
+        {
+            renderer.enabled = false;
+        }
+        BoxCollider collider = GetComponent<BoxCollider>();
+        if(collider != null)
+        {
+            collider.enabled = false;
+        }
+        if(internalIcon != null)
+        {
+            internalIcon.SetActive(false);
+        }
+
         playerObject = player;
         playerController = playerObject.GetComponent<PlayerController>();
         isActive = true;
@@ -31,10 +54,23 @@ public class Status_Base : MonoBehaviour
     public virtual void Tick()
     {
         buffTimer += Time.deltaTime;
+        if(buffTimer >= durationOfEffect)
+        {
+            EndBuff();
+        }
     }
 
     protected virtual void EndBuff()
     {
         isActive = false;
+        Destroy(transform.parent.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartBuff(other.gameObject);
+        }
     }
 }
